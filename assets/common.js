01b329ceea821a702498f22edc5ed2add892eb3d
Andrey Bondarenko <andrey.bondarenko.devmrks@gmail.com>
2022-08-01 15:31:22 +0300

    order

--- assets/common.js
+++ assets/common.js
@@ -1,20 +1,20 @@
- document.addEventListener("DOMContentLoaded", () => {
-   const getUserUrl = window.location.href;
-         findVclid = getUserUrl.split('vclid=');
-  
-   let getNoteBlock = document.querySelector('#main-cart-footer .cart__note'),
-       getVclid;
-  
-   if(findVclid[1]){
-     getVclid = findVclid[1];
-          
-     /****** Add to local storege ******/
-     localStorage.setItem('userVclid', getVclid);
+document.addEventListener("DOMContentLoaded", () => {
+  const getUserUrl = window.location.href;
+  findVclid = getUserUrl.split("vclid=");
+
+  let getNoteBlock = document.querySelector("#Details-CartDrawer .cart__note"),
+    getVclid;
+
+  if (findVclid[1]) {
+    getVclid = findVclid[1];
+
+    /****** Add to local storege ******/
+    localStorage.setItem("userVclid", getVclid);
   }
-  
-  if(getNoteBlock && localStorage.getItem('userVclid') !== null){
-     let check = localStorage.getItem('userVclid');
-     getNoteBlock.querySelector('textarea').value = `VcLID: ${check}`;
-     localStorage.removeItem('userVclid');
+
+  if (getNoteBlock && localStorage.getItem("userVclid") !== null) {
+    let check = localStorage.getItem("userVclid");
+    getNoteBlock.querySelector("textarea").value = `VcLID: ${check}`;
+    //localStorage.removeItem("userVclid");
   }
 });
