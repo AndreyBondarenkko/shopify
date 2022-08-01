e994703bc97a4b6eeff72384c18a64907b429251
Andrey Bondarenko <andrey.bondarenko.devmrks@gmail.com>
2022-08-01 12:43:40 +0300

    noties

--- assets/common.js
+++ assets/common.js
@@ -4,16 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let getNoteBlock = document.querySelector("#Details-CartDrawer .cart__note"),
     getVclid;
+
   if (findVclid[1]) {
     getVclid = findVclid[1];
-
     /****** Add to local storege ******/
     localStorage.setItem("userVclid", getVclid);
   }
 
-  if (getNoteBlock && localStorage.getItem("userVclid") !== null) {
+  if (getNoteBlock && localStorage.getItem("userVclid")) {
     let check = localStorage.getItem("userVclid");
-    getNoteBlock.querySelector("textarea").value = `VcLID: ${check}`;
-    localStorage.removeItem("userVclid");
+    getNoteBlock.querySelector("textarea").value = `Vclid: ${check}`;
+    //localStorage.removeItem("userVclid");
   }
 });
